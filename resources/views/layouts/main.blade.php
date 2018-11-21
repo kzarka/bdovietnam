45f9c4234827e4713c548fd9f48736b9542fd029
Huy Tien <nhtienptit@gmail.com>
2018-11-21 17:02:22 +0700

    a

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>@yield('title')</title>
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+		<!--
+		CSS
+		============================================= -->
+		<link rel="stylesheet" href="css/linearicons.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		<link rel="stylesheet" href="css/nice-select.css">
+		<link rel="stylesheet" href="css/animate.min.css">
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/jquery-ui.css">
+		<link rel="stylesheet" href="css/main.css">
+		@yield('css')
+	</head>
+	<body>
+		<header>
+			@include('include.header')
+		</header>
+		
+		<div class="site-main-container">
+			@yield('content')
+		</div>
+		
+		<!-- start footer Area -->
+		<footer class="footer-area section-gap">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 single-footer-widget">
+						<h4>Top Products</h4>
+						<ul>
+							<li><a href="#">Managed Website</a></li>
+							<li><a href="#">Manage Reputation</a></li>
+							<li><a href="#">Power Tools</a></li>
+							<li><a href="#">Marketing Service</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-2 col-md-6 single-footer-widget">
+						<h4>Quick Links</h4>
+						<ul>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Brand Assets</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Terms of Service</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-2 col-md-6 single-footer-widget">
+						<h4>Features</h4>
+						<ul>
+							<li><a href="#">Jobs</a></li>
+							<li><a href="#">Brand Assets</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Terms of Service</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-2 col-md-6 single-footer-widget">
+						<h4>Resources</h4>
+						<ul>
+							<li><a href="#">Guides</a></li>
+							<li><a href="#">Research</a></li>
+							<li><a href="#">Experts</a></li>
+							<li><a href="#">Agencies</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 single-footer-widget">
+						<h4>Instragram Feed</h4>
+						<ul class="instafeed d-flex flex-wrap">
+							<li><img src="img/i1.jpg" alt=""></li>
+							<li><img src="img/i2.jpg" alt=""></li>
+							<li><img src="img/i3.jpg" alt=""></li>
+							<li><img src="img/i4.jpg" alt=""></li>
+							<li><img src="img/i5.jpg" alt=""></li>
+							<li><img src="img/i6.jpg" alt=""></li>
+							<li><img src="img/i7.jpg" alt=""></li>
+							<li><img src="img/i8.jpg" alt=""></li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer-bottom row align-items-center">
+					<p class="footer-text m-0 col-lg-8 col-md-12">
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="" target="_blank"></a></p>
+					<div class="col-lg-4 col-md-12 footer-social">
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-dribbble"></i></a>
+						<a href="#"><i class="fa fa-behance"></i></a>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- End footer Area -->
+		<script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+		<script src="{{ asset('js/easing.min.js') }}"></script>
+		<script src="{{ asset('js/hoverIntent.js') }}"></script>
+		<script src="{{ asset('js/superfish.min.js') }}"></script>
+		<script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
+		<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+		<script src="{{ asset('js/mn-accordion.js') }}"></script>
+		<script src="{{ asset('js/jquery-ui.js') }}"></script>
+		<script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
+		<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+		<script src="{{ asset('js/mail-script.js') }}"></script>
+		<script src="{{ asset('js/main.js') }}"></script>
+		@yield('js')
+	</body>
+</html>
