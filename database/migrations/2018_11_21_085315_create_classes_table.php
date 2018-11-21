799454ab1b7dde755d9b4c8a4832f94a1fa08802
evanthinh <socbay_ck@yahoo.com>
2018-11-21 18:08:07 +0700

    Init

---
+++ database/migrations/2018_11_21_085315_create_classes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->nullable();
+            $table->text('desc_normal')->nullable();
+            $table->text('desc_awaken')->nullable();
+            $table->tinyInteger('has_awk')->default(0);
+            $table->string('normal_img', 200)->nullable();
+            $table->string('awaken_img', 200)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
