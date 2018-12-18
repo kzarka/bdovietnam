2eb208708567866967d0d7269d15f847a60e0f3d
Huy Tien <nhtienptit@gmail.com>
2018-12-18 14:56:56 +0700

    Fix to slug

--- public/admin/js/posts.js
+++ public/admin/js/posts.js
@@ -104,11 +104,13 @@ $(document).ready(function() {
         str = str.replace(/^\s+|\s+$/g, ''); // trim
         str = str.toLowerCase();
         // remove accents, swap ñ for n, etc
-        var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
-        var to   = "aaaaaeeeeeiiiiooooouuuunc------";
-        for (var i=0, l=from.length ; i<l ; i++) {
-            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-        }
+        str = str.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+        str = str.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+        str = str.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+        str = str.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+        str = str.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+        str = str.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+        str = str.replace(/đ/gi, 'd');
 
         str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
         .replace(/\s+/g, '-') // collapse whitespace and replace by -
