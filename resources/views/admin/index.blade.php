75f5ceeb58e663205e176472e1a7cc25780702f8
Huy Tien <nhtienptit@gmail.com>
2019-01-02 09:04:22 +0700

    Admin side

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,41 +7,7 @@
     <div class="box">                
     <!-- /.box-header -->
         <div class="box-body">
-            <table id="example2" class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>業態</th>
-                        <th>サイネジ名</th>
-                        <th>表示位置</th>
-                        <th>適用開始日</th>
-                        <th>適用終了日</th>
-                        <th>登録日</th>
-                        <th>同期成功数</th>
-                        <th>同期失収数</th>
-                        <th>登録者</th>
-                        <th>画像表示</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Hamazushi’s</td>
-                        <td>サイネジ1</td>
-                        <td>2</td>
-                        <td>2018/10/15</td>
-                        <td>2018/10/15</td>
-                        <td>2018/11/15</td>
-                        <td>245</td>
-                        <td><a href="#">4</a></td>
-                        <td>namiki</td>
-                        <td><a href="#">表示</a></td>
-                        <td>
-                            <a href="#" title="" data-toggle="modal" data-target="#modal-edit-media-files"><i class="fa fa-cog"></i> </a>
-                            <a href="#" title=""><i class="fa fa-trash"></i></a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            
         </div>
     <!-- /.box-body -->
     </div>
--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -121,16 +121,7 @@
 
 <!-- page script -->
 <script>
-    $(function () {
-      $('#example2').DataTable({
-        'paging'      : true,
-        'lengthChange': false,
-        'searching'   : false,
-        'ordering'    : true,
-        'info'        : true,
-        'autoWidth'   : false
-      })
-    })
+    
 </script>
 
 
